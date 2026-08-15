4b95774b709bb53149a5f4161382f9cf6a0f866e
mahdi911110 <gorbany53@gmail.com>
2026-08-15 23:37:11 +0330

    refactor(ExpenseItems): simplify JSX and add test IDs

--- src/components/ExpenseItems.jsx
+++ src/components/ExpenseItems.jsx
@@ -55,201 +55,108 @@ export function ExpenseItems({ listItem, deleteBtn, setList, isPersian }) {
     setInputDate(listItem.date);
     setInputKeyWord(listItem.keyWord);
   }
-  if (isPersian) {
-    return (
-      <div className='expense-item'>
-        <div className='expense-actions-fa'>
-          <button
-            className='btn-delete'
-            onClick={() => {
-              deleteBtn(listItem.id)
-            }}>
-            <img
-              className='img-delete'
-              src="/delete.svg"
-              alt="delete"
-            />
-          </button>
-          {isEdit
-          ?
-            <button
-              className='btn-save'
-              onClick={() => {
-                handleEditSave(listItem.id)
-              }}
-            >
-              <img
-                className='img-save'
-                src="/save.svg" alt="save"
-              />
-            </button>
-          :
-            <button
-              className='btn-edit'
-              onClick={handleEditSave}
-            >
-              <img
-                className='img-edit'
-                src="/edit.svg"
-                alt="edit"
-              />
-            </button>
-          }
-        </div>
+  return (
+    <div className='expense-item' data-testid='expense-item'>
         {isEdit
         ? 
           <input
-            className="input-change-keyword"
+            className="input-change-title"
+            data-testid="input-change-title"
             type="text"
-            placeholder="تغییر کلیدواژه"
-            onChange={saveInputKeyWord}
-            value={inputKeyWord}
+            placeholder={isPersian ? 'تغییر عنوان' : "Change title"}
+            value={inputText}
+            onChange={saveInputText}
           />
         :
-          <div className='expense-keyword-fa'>
-            {listItem.keyWord}
-          </div>
-        }
-        {isEdit
-        ? 
-          <input
-            className="input-change-date"
-            type="date"
-            onChange={saveInputDate}
-            value={inputDate}
-          />
-        :
-          <div className='expense-date-fa'>
-            {listItem.date}
+          <div className="expense-title" data-testid="expense-title">
+            {listItem.title}
           </div>
         }
         {isEdit
         ? 
           <input
             className="input-change-price"
+            data-testid="input-change-price"
             type="text"
-            placeholder="تغییر مبلغ"
+            placeholder={isPersian ? "تغییر مبلغ" : "Change price"}
             value={inputPrice}
             onChange={saveInputPrice}
           />
         :
-          <div className='expense-amount-fa'>
+          <div className='expense-amount' data-testid="expense-amount">
             {formatMoney(listItem.priceCents)}
           </div>
         }
-          {isEdit
-          ? 
-            <input
-              className="input-change-title"
-              type="text"
-              placeholder="تغییر عنوان"
-              value={inputText}
-              onChange={saveInputText}
-            />
-          :
-            <div className="expense-title-fa">
-              {listItem.title}
-            </div>
-          }
-      </div>
-    );
-  } else {
-    return (
-      <div className='expense-item'>
-          {isEdit
-          ? 
-            <input
-              className="input-change-title"
-              type="text"
-              placeholder="Change title"
-              value={inputText}
-              onChange={saveInputText}
-            />
-          :
-            <div className="expense-title">
-              {listItem.title}
-            </div>
-          }
-          {isEdit
-          ? 
-            <input
-              className="input-change-price"
-              type="text"
-              placeholder="Change price"
-              value={inputPrice}
-              onChange={saveInputPrice}
-            />
-          :
-            <div className='expense-amount'>
-              {formatMoney(listItem.priceCents)}
-            </div>
-          }
-          {isEdit
-          ? 
-            <input
-              className="input-change-date"
-              type="date"
-              onChange={saveInputDate}
-              value={inputDate}
-            />
-          :
-            <div className='expense-date'>
-              {listItem.date}
-            </div>
-          }
-          {isEdit
-          ? 
-            <input
-              className="input-change-keyword"
-              type="text"
-              placeholder="Change keyword"
-              onChange={saveInputKeyWord}
-              value={inputKeyWord}
-            />
-          :
-            <div className='expense-keyword'>
-              {listItem.keyWord}
-            </div>
-          }
-        <div className='expense-actions'>
-          {isEdit
-          ?
-            <button
-              className='btn-save'
-              onClick={() => {
-                handleEditSave(listItem.id)
-              }}
-            >
-              <img
-                className='img-save'
-                src="/save.svg" alt="save"
-              />
-            </button>
-          :
-            <button
-              className='btn-edit'
-              onClick={handleEditSave}
-            >
-              <img
-                className='img-edit'
-                src="/edit.svg"
-                alt="edit"
-              />
-            </button>
-          }
+        {isEdit
+        ? 
+          <input
+            className="input-change-date"
+            data-testid="input-change-date"
+            type="date"
+            onChange={saveInputDate}
+            value={inputDate}
+          />
+        :
+          <div className='expense-date' data-testid="expense-date">
+            {listItem.date}
+          </div>
+        }
+        {isEdit
+        ? 
+          <input
+            className="input-change-keyword"
+            data-testid="input-change-keyword"
+            type="text"
+            placeholder={isPersian ? "تغییر کلیدواژه" : "Change keyword"}
+            onChange={saveInputKeyWord}
+            value={inputKeyWord}
+          />
+        :
+          <div className='expense-keyword' data-testid="expense-keyword">
+            {listItem.keyWord}
+          </div>
+        }
+      <div className='expense-actions'>
+        {isEdit
+        ?
           <button
-            className='btn-delete'
+            className='btn-save'
+            data-testid="btn-save"
             onClick={() => {
-              deleteBtn(listItem.id)
-            }}>
+              handleEditSave(listItem.id)
+            }}
+          >
             <img
-              className='img-delete'
-              src="/delete.svg"
-              alt="delete"
+              className='img-save'
+              src="/save.svg" alt="save"
             />
           </button>
-        </div>
+        :
+          <button
+            className='btn-edit'
+            data-testid="btn-edit"
+            onClick={handleEditSave}
+          >
+            <img
+              className='img-edit'
+              src="/edit.svg"
+              alt="edit"
+            />
+          </button>
+        }
+        <button
+          className='btn-delete'
+          data-testid="btn-delete"
+          onClick={() => {
+            deleteBtn(listItem.id)
+          }}>
+          <img
+            className='img-delete'
+            src="/delete.svg"
+            alt="delete"
+          />
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
